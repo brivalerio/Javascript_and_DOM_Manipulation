bee0db724c2be3bd9cc4e2ad2083c81ca8e64fbf
brivalerio <brivalerio@gmail.com>
2019-07-27 22:50:22 -0700

    saving progress

--- static/js/app.js
+++ static/js/app.js
@@ -4,8 +4,14 @@ console.log(data);
 
 // YOUR CODE HERE!
 var tbody = d3.select("tbody");
+// console.log(tbody);
 
-console.log(tbody);
+data.forEach((ufoData) => {
+    var row = tbody.append("tr");
+    Object.entries(ufoData).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
 
-
-   
+var button = d3.select("#filter-btn");
